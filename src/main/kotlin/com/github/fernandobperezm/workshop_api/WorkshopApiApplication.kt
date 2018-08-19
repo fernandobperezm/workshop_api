3f698b3bd369e35cbd2c2340b6f3d8186a5fb51e
Fernando Pérez <fperezmaurera@gmail.com>
2018-08-19 00:06:37 -0400

    Initial commit. This setup was generated inside IntelliJ, by selecting a new Spring Initializr project and following the instructions for new spring boot kotlin projects inside the 'Building web applications with spring boot and kotlin' tutorial.

---
+++ src/main/kotlin/com/github/fernandobperezm/workshop_api/WorkshopApiApplication.kt
@@ -0,0 +1,11 @@
+package com.github.fernandobperezm.workshop_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkshopApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorkshopApiApplication>(*args)
+}
